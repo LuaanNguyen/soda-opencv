049116d5f65fa5348f2d5da9ffcd5a8d897faa8f
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-21 20:06:45 -0700

    Tested the model

---
+++ yolo.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import cv2 
+
+
+model = YOLO("yolov8s.pt")
+
+img = cv2.imread("test.jpg")
+
+results = model(img)[0]
+
+results.save(filename="output.jpg")
+
+plotted_img = results.plot()
+cv2.imwrite("output.jpg", plotted_img)
+
+cv2.destroyAllWindows()
